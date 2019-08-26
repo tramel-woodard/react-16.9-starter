776ec937497dcbe683ee6fcf3e8fbb474170b077
Tramel Woodard <20341756+tramel-woodard@users.noreply.github.com>
2019-08-27 03:59:10 +0800

    Adding index JavaScript, HTML, App component and app stylesheet.

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component} from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h2>React 16.9 Starter</h2>
+            </div>
+        );
+    }
+}
+
+export default App;
